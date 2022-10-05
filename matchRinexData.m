58941abc409f9ed61dec9133646eda7b29cbe960
LandonBoyd <lrb0030@auburn.edu>
2022-10-05 11:39:57 -0500

    Don't even read the commit messages, everythings getting overhauled

---
+++ matchRinexData.m
@@ -0,0 +1,132 @@
+function output = matchRinexData(data,obsTypes)
+
+% Set logical limits
+phaseLim = 150000000;
+codeLim = 30000000;
+SNRLim = 70;
+LLILim = 10;
+
+% C1 and P1 are duplicates and mess up the counting, remove them
+for ii = 1:length(obsTypes)-1
+    arr1 = obsTypes{ii};
+    arr2 = obsTypes{ii+1};
+
+    % Mark duplicates. If theres a P-C duo on the same band then delete
+    % one
+    if (arr1(1) == 'C' && arr2(1) == 'P') && (str2double(arr1(2)) == str2double(arr2(2)))
+        obsTypes{ii+1} = 'erase';
+    end
+end
+
+obsTypes(ismember(obsTypes,'erase')) = [];
+
+% Count through the data
+for ii = 1:length(obsTypes)
+
+    found = 0;
+    switch obsTypes{ii}
+
+        case 'L1' % L1 phase measurement
+            % Find the L1 phase in the data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= phaseLim) && (data(jj) >= codeLim)
+                    output.L1phase = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case 'L2'
+            % Find the L2 phase in the data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= phaseLim) && (data(jj) >= codeLim)
+                    output.L2phase = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case 'L5'
+            % Find the L5 phase in the data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= phaseLim) && (data(jj) >= codeLim)
+                    output.L5phase = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case {'C1','P1'}
+            % Find the L1 psuedorange data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= codeLim) && (data(jj) >= SNRLim)
+                    output.L1psr = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case {'C2','P2'}
+            % Find the L2 psuedorange data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= codeLim) && (data(jj) >= SNRLim)
+                    output.L2psr = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case {'C5','P5'}
+            % Find the L2 psuedorange data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= codeLim) && (data(jj) >= SNRLim)
+                    output.L5psr = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case 'S1'
+            % Find the L1 SNR data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= SNRLim) && (data(jj) >= LLILim)
+                    output.L1SNR = data(jj);
+                    found = 1;
+                    break
+                end
+            end
+
+        case 'S2'
+            % Find the L1 SNR data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= SNRLim) && (data(jj) >= LLILim)
+                    output.L2SNR = data(jj)
+                    found = 1;
+                    break
+                end
+            end
+
+        case 'S5'
+            % Find the L1 SNR data and remove it
+            for jj = 1:length(data)
+                if (data(jj) <= SNRLim) && (data(jj) >= LLILim)
+                    output.L5SNR = data(jj)
+                    found = 1;
+                    break
+                end
+            end
+
+        otherwise
+            error('Format specifier ' + obsTypes{ii} + ' not recognized')
+
+    end
+
+    % Delete entry we just processed if we found a match
+    if found
+        data(jj) = [nan];
+    end
+
+end
+
+
+end
--- rinexDataRead.m
+++ rinexDataRead.m
@@ -6,9 +6,9 @@ fileID = fopen(filename,'r');
 headerCount = 0;
 while 1
    headerCount = headerCount+1;
-   line = fgetl(fileID);
+   epochHeaderLine1 = fgetl(fileID);
 
-   if contains(line,"END OF HEADER")
+   if contains(epochHeaderLine1,"END OF HEADER")
        break;	
    end
 end
@@ -17,82 +17,165 @@ end
 % of claimed observables, it would appear that each band reduces this
 % number of actual entries in data by 1. I think this is because C1/P1 is
 % listed as a single entry
-numFields = length(obsTypes);
+
+% Alternate approach, just count three extra fields for each band (carrier, rangea and signal strength)
+numFields = 0;%length(obsTypes);
 if ismember("L1",obsTypes)
-    numFields = numFields - 1;
+    numFields = numFields + 3;
 end
 if ismember("L2",obsTypes)
-    numFields = numFields - 1;
+    numFields = numFields + 3;
 end
 if ismember("L5",obsTypes)
-    numFields = numFields - 1;
+    numFields = numFields  + 3;
 end
 
 gnssData = {};
 count = 1;
+
 while ~feof(fileID)
 
     % Arrange data in a cell of structs
     currentData = struct;
+    currentData.obsTypes = obsTypes;
+
+    % Storage for observables later
+    currentData.GPSObservables = [];
+    currentData.GLOObservables = [];
+    currentData.GALObservables = [];
 
     % Read in the epoch time and listed satellites
-    line = split(fgetl(fileID));
+    epochHeaderLine1 = split(fgetl(fileID));
 
     % All time values from epoch
-    currentData.year = str2double(line{2});
-    currentData.month = str2double(line{3});
-    currentData.day = str2double(line{4});
-    currentData.hour = str2double(line{5});
-    currentData.minute = str2double(line{6});
-    currentData.second = str2double(line{7});
+    currentData.year = str2double(epochHeaderLine1{2});
+    currentData.month = str2double(epochHeaderLine1{3});
+    currentData.day = str2double(epochHeaderLine1{4});
+    currentData.hour = str2double(epochHeaderLine1{5});
+    currentData.minute = str2double(epochHeaderLine1{6});
+    currentData.second = str2double(epochHeaderLine1{7});
 
-    % Try and take apart the satellite term. For now I can only handle GPS
-    % satellites, no Galileo or Glonass
-    svList = split(replace(char(line{end}),'G',' '));
-    
-    % The first number of this term is the number of satellites observed,
-    % remove it from the array
-    numSats = str2double(svList{1});
-    svList(1) = [];
+    % Second element is number of observables. It is the first two
+    % characters of the last element in the epochHeaderLine
+    numSats = epochHeaderLine1{end};
+    numSats = str2double(numSats(1:2));
 
-    % Store observables in a table later. The extra column is to store SV
-    % PRN later on
-    observables = zeros(length(svList),numFields+1);
+    % Constellation description is the last element of the array, and is
+    % possibly continues on the next line
+    constellation = epochHeaderLine1{end};
 
-    % Count through all of the observables for each satellite in the epoch
-    for ii = 1:numSats
+    % If more than 9 observables, must grab two lines instead of one
+    if numSats > 9
+        epochHeaderLine2 = split(fgetl(fileID));
 
-        obsCount = 0    ;
-        lineCount = 1   ;
-
-        % Keep counting until we have all of the measurements we expect
-        while obsCount < numFields
-
-            % Get a line to read
-            line = split(fgetl(fileID));
-
-            % I'm not sure what the special number on the first line is but
-            % I will know one day and then this part will have meaning
-            if lineCount == 1
-                line{3} = '';
-            end
-
-            % Count through all nonzero elements in order based of obsTypes
-            for jj = 1:length(line)
-
-                % If not an empty character vector or a letter then add to
-                % our data matrix
-                if ~(isempty(line{jj}))
-                    observables(ii,obsCount+2) = str2double(line{jj});
-                    obsCount = obsCount + 1;
-                end
-            end
-
-            lineCount = lineCount + 1;
-    
-        end
-        currentData.observables = observables;
+        % Join our additional constellation data to what we already have
+        constellation = [constellation,epochHeaderLine2{end}];
     end
+
+    % Read in the constellation term and save
+    satDescription = readConstellation(constellation);
+    currentData.constellation = satDescription;
+
+    % Set flags
+    GPSTrue = ~isempty(satDescription.GPS);
+    GLOTrue = ~isempty(satDescription.GLO);
+    GALTrue = ~isempty(satDescription.GAL);
+
+    % A primary assumption here is that the CORS file lists in order GPS,
+    % GLONASS and then Galileo
+    for ii = 1:length(satDescription.GPS)
+        % Get the two lines of data and parse
+        dataLine1 = split(fgetl(fileID));
+        dataLine2 = split(fgetl(fileID));
+
+        % For some reason the first element is always an empty character
+        % array. Also I dont knwo what the number at the beginning of the
+        % observation list is but I can use it later if I want it
+        dataLine1 = dataLine1(2:end);
+        dataLine2 = dataLine2(2:end);
+
+
+        % Convert to a number
+        obsData = str2double([dataLine1;dataLine2]);
+
+        % Remove the loss of lock indicator
+        obsData(obsData < 10) = [];
+
+
+        % Store
+        currentData.GPSObservables(ii,:) = obsData';
+
+    end
+
+    for ii = 1:length(satDescription.GLO)
+        % Get the two lines of data and parse
+        dataLine1 = split(fgetl(fileID));
+        dataLine2 = split(fgetl(fileID));
+
+        % For some reason the first element is always an empty character
+        % array. Also I dont knwo what the number at the beginning of the
+        % observation list is but I can use it later if I want it
+        dataLine1 = dataLine1(2:end);
+        dataLine2 = dataLine2(2:end);
+
+
+        % Convert to a number
+        obsData = str2double([dataLine1;dataLine2]);
+
+        % Remove the loss of lock indicator
+        obsData(obsData < 10) = [];
+
+
+        % Store
+        currentData.GLOObservables(ii,:) = obsData';
+
+    end
+
+
+
+
+
+%     % Store observables in a table later. The extra column is to store SV
+%     % PRN later on
+%     observables = zeros(length(svList),numFields+1);
+
+    
+
+
+%     % Count through all of the observables for each satellite in the epoch
+%     for ii = 1:numSats
+% 
+%         obsCount = 0    ;
+%         lineCount = 1   ;
+% 
+%         % Keep counting until we have all of the measurements we expect
+%         while obsCount < numFields
+% 
+%             % Get a line to read
+%             line = split(fgetl(fileID));
+% 
+%             % I'm not sure what the special number on the first line is but
+%             % I will know one day and then this part will have meaning
+%             if lineCount == 1
+%                 line{3} = '';
+%             end
+% 
+%             % Count through all nonzero elements in order based of obsTypes
+%             for jj = 1:length(line)
+% 
+%                 % If not an empty character vector or a letter then add to
+%                 % our data matrix
+%                 if ~(isempty(line{jj}))
+%                     observables(ii,obsCount+2) = str2double(line{jj});
+%                     obsCount = obsCount + 1;
+%                 end
+%             end
+% 
+%             lineCount = lineCount + 1;
+%     
+%         end
+%         currentData.observables = observables;
+%     end
     
     gnssData{count} = currentData;
     count = count + 1
